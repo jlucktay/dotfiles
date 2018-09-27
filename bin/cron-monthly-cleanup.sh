d8b059c0b240032f702a14f05d48953f7478f502
James Lucktaylor <jlucktay+github@gmail.com>
2018-09-27 15:46:04 +0100

    Cleaned up cleanup script :inception:

--- bin/cron-monthly-cleanup.sh
+++ bin/cron-monthly-cleanup.sh
@@ -2,15 +2,16 @@
 set -euo pipefail
 IFS=$'\n\t'
 
-CronLog="/Users/jameslucktaylor/log/cron.$(gdate '+%Y%m%d.%H%M%S.%N%z').log"
+CronLogDir="/Users/jameslucktaylor/log"
+mkdir -p $CronLogDir
 
-echo "--- brew cleanup" >> "$CronLog"
-brew cleanup >> "$CronLog" 2>&1
+CronLog="${CronLogDir}/cron.$(gdate '+%Y%m%d.%H%M%S.%N%z').log"
 
-open --background -a Docker
-while ! docker system info > /dev/null 2>&1; do
-    sleep 1
+{ echo "--- 'brew cleanup'"; brew cleanup; } >> "$CronLog" 2>&1
+
+{ echo "--- Starting Docker..."; open --background -a Docker; } >> "$CronLog"
+until docker system info > /dev/null 2>&1; do
+    sleep 0.1s
 done
 
-echo "--- docker system prune --all --force" >> "$CronLog"
-docker system prune --all --force >> "$CronLog" 2>&1
+{ echo "--- 'docker system prune --all --force'"; docker system prune --all --force; } >> "$CronLog" 2>&1
