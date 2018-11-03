c13b1cb75a50e98f44c4125fcbd35aea5df09b9f
James Lucktaylor <jlucktay+github@gmail.com>
2018-11-03 11:59:57 +1100

    Tweaking logic for better behaviour

--- bin/homemaker/list-homebrew.sh
+++ bin/homemaker/list-homebrew.sh
@@ -15,21 +15,21 @@ function processHomebrewLists () {
     # echo "Parameter #2 is the filename to put the new list in: '$2'"
     # echo "Parameter #3 is the filename to archive the old list to: '$3'"
 
+    echo "# $Now" > "$3"
+    eval "$1" >> "$3"
+
     if [ -f "$2" ]; then
-        mv -iv "$2" "$3"
-    fi
-
-    echo "# $Now" > "$2"
-    eval "$1" >> "$2"
-
-    if [ -f "$3" ]; then
         # Compare the old archive file with the newly-created file
         DiffResult=$(diff --brief <(tail -n +2 "$2") <(tail -n +2 "$3") | wc -l)
 
         if ! ((DiffResult)); then
-            # No difference, so remove the archive
+            # No difference, so delete the new file
             rm -fv "$3"
+        else
+            rm -fv "$2"
         fi
+    else
+        mv -iv "$3" "$2"
     fi
 }
 
