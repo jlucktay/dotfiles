512b4b0a9e34aeb1819a70de6785116dfaa3c925
James Lucktaylor <jlucktay+github@gmail.com>
2018-11-14 10:01:58 +0000

    Enacted shellharden recommendations

--- homemaker.sh
+++ homemaker.sh
@@ -1,13 +1,21 @@
 #!/usr/bin/env bash
-set -euo pipefail
+# Thank you: https://github.com/anordal/shellharden/blob/master/how_to_do_things_safely_in_bash.md#how-to-begin-a-bash-script
+if test "$BASH" = "" || "$BASH" -uc "a=();true \"\${a[@]}\"" 2>/dev/null; then
+    # Bash 4.4, Zsh
+    set -euo pipefail
+else
+    # Bash 4.3 and older chokes on empty arrays with set -u.
+    set -eo pipefail
+fi
+shopt -s nullglob globstar
 IFS=$'\n\t'
 
 ScriptDirectory="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null && pwd )"
 HomemakerTask=${1:-"default"}
-ExtraArguments=""
+HomemakerCommand="homemaker"
 
 if [ "$HomemakerTask" == "git" ]; then
-    ExtraArguments="--clobber"
+    HomemakerCommand="$HomemakerCommand --clobber"
 fi
 
-homemaker $ExtraArguments --task="$HomemakerTask" --verbose config.toml "$ScriptDirectory"
+eval "$HomemakerCommand" --task="$HomemakerTask" --verbose config.toml "$ScriptDirectory"
