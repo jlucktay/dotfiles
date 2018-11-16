d3dc6080e0262194543dd61646c070e5940eb329
James Lucktaylor <jlucktay+github@gmail.com>
2018-11-16 16:43:41 +0000

    Wired up another list to cover VSCode extensions

---
+++ bin/homemaker/list-vscode.sh
@@ -0,0 +1,15 @@
+#!/usr/bin/env bash
+set -euo pipefail
+IFS=$'\n\t'
+
+ScriptDirectory="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null && pwd )"
+
+# shellcheck source=/dev/null
+. "$(realpath "$ScriptDirectory/func.processList.sh")"
+
+Now=$(date +%Y%m%d.%H%M%S.%N%z)
+
+VsCodeList=$(realpath "$ScriptDirectory/../../list.vscode.txt")
+VsCodeListArchive=$(realpath "$ScriptDirectory/../../list.vscode.$Now.txt")
+
+processList "$Now" "code --list-extensions | sort -f" "$VsCodeList" "$VsCodeListArchive"
