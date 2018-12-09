75f9e7ac34271d63c123781f3f76f5363d2c2b11
James Lucktaylor <jlucktay+github@gmail.com>
2018-12-09 22:17:15 +0000

    Added list of taps to Homebrew lists, and updated all

--- bin/homemaker/list-homebrew.sh
+++ bin/homemaker/list-homebrew.sh
@@ -9,10 +9,13 @@ ScriptDirectory="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null && pwd )"
 
 Now=$(date +%Y%m%d.%H%M%S.%N%z)
 
+HomebrewTapList=$(realpath "$ScriptDirectory/../../list.brew.tap.txt")
+HomebrewTapListArchive=$(realpath "$ScriptDirectory/../../list.brew.tap.$Now.txt")
 HomebrewList=$(realpath "$ScriptDirectory/../../list.brew.txt")
 HomebrewListArchive=$(realpath "$ScriptDirectory/../../list.brew.$Now.txt")
 HomebrewCaskList=$(realpath "$ScriptDirectory/../../list.brew.cask.txt")
 HomebrewCaskListArchive=$(realpath "$ScriptDirectory/../../list.brew.cask.$Now.txt")
 
+processList "$Now" "brew tap" "$HomebrewTapList" "$HomebrewTapListArchive"
 processList "$Now" "brew list -1" "$HomebrewList" "$HomebrewListArchive"
 processList "$Now" "brew cask list -1" "$HomebrewCaskList" "$HomebrewCaskListArchive"
