0cdd116f5efc87f1053ede27588a1994035e4b1e
James Lucktaylor <jlucktay+github@gmail.com>
2019-08-06 09:57:28 +0100

    Added regions, removed Name filter (private lab).

--- bin/aws-my-ec2-status.sh
+++ bin/aws-my-ec2-status.sh
@@ -2,13 +2,26 @@
 set -euo pipefail
 IFS=$'\n\t'
 
-# todo: roll through all regions with $(awsregions) and '--region'
-aws ec2 describe-instances --filters "Name=tag:Owner,Values=james.lucktaylor" |
-    jq '.Reservations[].Instances[] |
-        {
-            n: (.Tags[] | select(.Key == "Name") | .Value),
-            id: .InstanceId,
-            az: .Placement.AvailabilityZone,
-            ip: .PublicIpAddress,
-            up: .State.Name
-        }'
+if ! command -v aws > /dev/null; then
+    echo "'aws' not found! Please install: https://aws.amazon.com/cli/"
+    exit 1
+fi
+
+if ! command -v jq > /dev/null; then
+    echo "'jq' not found! Please install: https://stedolan.github.io/jq/download/"
+    exit 1
+fi
+
+for Region in $(awsregions); do
+    echo "Region: $Region"
+
+    aws ec2 describe-instances --region "$Region" |
+        jq '.Reservations[].Instances[] |
+            {
+                n: (.Tags[] | select(.Key == "Name") | .Value),
+                id: .InstanceId,
+                az: .Placement.AvailabilityZone,
+                ip: .PublicIpAddress,
+                up: .State.Name
+            }'
+done
