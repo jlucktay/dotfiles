ba79bd084535c936742416fe55d3bbc6497ad1c4
James Lucktaylor <jlucktay+github@gmail.com>
2019-10-14 15:15:41 +0100

    Add time taken.

--- rsync_tmbackup/run.sh
+++ rsync_tmbackup/run.sh
@@ -7,8 +7,13 @@ ScriptDirectory="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd
 Flags=$(rsync_tmbackup.sh --rsync-get-flags)
 Flags+=" --no-devices --no-specials"
 
+TimeStart=$(gdate +%s%N)
+
 rsync_tmbackup.sh \
     --rsync-set-flags "$Flags" \
     /Users/jameslucktaylor \
     /Volumes/Sgte-ExFAT/home \
     "${ScriptDirectory}/exclude.txt"
+
+TimeTaken=$((($(gdate +%s%N) - TimeStart)/1000000))
+echo "Time taken: ${TimeTaken}ms"
