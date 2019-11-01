74ddf378eef78e382fc479b749c4df0299da1cfa
James Lucktaylor <jlucktay+github@gmail.com>
2019-11-01 08:44:08 +0000

    Add GCP SDK and update more VSCode config.

---
+++ .config/profile.d/306.bash-gcloud.sh
@@ -0,0 +1,4 @@
+# https://formulae.brew.sh/cask/google-cloud-sdk
+# $ brew cask caveats google-cloud-sdk
+source '/usr/local/Caskroom/google-cloud-sdk/latest/google-cloud-sdk/path.bash.inc'
+source '/usr/local/Caskroom/google-cloud-sdk/latest/google-cloud-sdk/completion.bash.inc'
