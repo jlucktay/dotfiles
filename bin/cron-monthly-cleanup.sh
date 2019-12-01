d0df1a23e34fa913d777ca26a51cdb65c453ac13
James Lucktaylor <jlucktay+github@gmail.com>
2019-12-01 21:42:31 +0000

    Update with more validations and per Google style guide.

--- bin/cron-monthly-cleanup.sh
+++ bin/cron-monthly-cleanup.sh
@@ -2,16 +2,44 @@
 set -euo pipefail
 IFS=$'\n\t'
 
-CronLogDir="$HOME/log"
-mkdir -p "$CronLogDir"
+script_name=$( basename "${BASH_SOURCE[-1]}" )
+cron_log_dir="$HOME/log"
 
-CronLog="$CronLogDir/cron.$(gdate '+%Y%m%d.%H%M%S.%N%z').log"
+# Check for presence of other variables/tools
+command -v brew &>/dev/null || {
+    echo >&2: "$script_name: Homebrew has not been installed: https://brew.sh"
+    exit 1
+}
 
-{ echo "--- 'brew cleanup'"; brew cleanup; } >> "$CronLog" 2>&1
+command -v docker &>/dev/null || {
+    echo >&2: "$script_name: Docker has not been installed via Homebrew: https://formulae.brew.sh/cask/docker"
+    exit 1
+}
 
-{ echo "--- Starting Docker..."; open --background -a Docker; } >> "$CronLog"
-until docker system info > /dev/null 2>&1; do
+test -d /usr/local/Cellar/coreutils || {
+    echo >&2 "$script_name: 'coreutils' has not been installed via Homebrew: https://formulae.brew.sh/formula/coreutils"
+    exit 1
+}
+
+# Setup log and run cleanups
+gmkdir -pv "$cron_log_dir"
+cron_log="$cron_log_dir/cron.$( gdate '+%Y%m%d.%H%M%S.%N%z' ).log"
+
+{
+    echo "--- 'brew cleanup'"
+    brew cleanup
+} &>> "$cron_log"
+
+{
+    echo "--- Starting Docker..."
+    open --background -a Docker
+} >> "$cron_log"
+
+until docker system info &> /dev/null; do
     sleep 0.1s
 done
 
-{ echo "--- 'docker system prune --all --force'"; docker system prune --all --force; } >> "$CronLog" 2>&1
+{
+    echo "--- 'docker system prune --all --force'"
+    docker system prune --all --force
+} &>> "$cron_log"
