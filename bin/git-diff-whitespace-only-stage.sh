5d2ddb4103dc897e0e40c717236dfe69b1f42cf8
James Lucktaylor <jlucktay+github@gmail.com>
2019-12-10 20:24:15 +0000

    Add new helper script.

---
+++ bin/git-diff-whitespace-only-stage.sh
@@ -0,0 +1,12 @@
+#!/usr/bin/env bash
+set -euo pipefail
+shopt -s nullglob globstar
+IFS=$'\n\t'
+
+while read -r diff_file; do
+  diff_without_whitespace=$(git diff -w -U0 --word-diff-regex=[^[:space:]] "$diff_file")
+
+  if test -z "$diff_without_whitespace"; then
+    git add "$diff_file"
+  fi
+done < <(git diff --name-only)
