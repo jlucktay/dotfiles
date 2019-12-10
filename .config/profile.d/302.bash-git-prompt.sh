6a1cc9a14a1db6c3046789387753bc8e3192d2e1
James Lucktaylor <jlucktay+github@gmail.com>
2019-12-10 19:00:28 +0000

    Clean up prompt setup.

--- .config/profile.d/302.bash-git-prompt.sh
+++ .config/profile.d/302.bash-git-prompt.sh
@@ -1,10 +1,50 @@
-### Prompt setup
+## Prompt setup
 # Git info in the prompt
-if [ -f "$HOME/posh-git-prompt.sh" ]; then
-    # shellcheck disable=SC1090
-    source "$HOME/posh-git-prompt.sh"
-
-    PROMPT_COMMAND='__posh_git_ps1 "\[\033[38;5;14m\]\u\[$(tput sgr0)\]\[\033[38;5;15m\]@\[$(tput sgr0)\]\[\033[38;5;13m\]\H\[$(tput sgr0)\]\[\033[38;5;15m\] [\[$(tput sgr0)\]\[\033[38;5;11m\]\W\[$(tput sgr0)\]\[\033[38;5;15m\]] \$? " "\n\[$(tput sgr0)\]\[\033[38;5;10m\]\t\[$(tput sgr0)\]\[\033[38;5;15m\] \[$(tput sgr0)\]\[\033[38;5;9m\]\\$\[$(tput sgr0)\]\[\033[38;5;15m\] \[$(tput sgr0)\]"'$PROMPT_COMMAND
-else
-    echo "'$HOME/posh-git-prompt.sh' is not available: https://github.com/lyze/posh-git-sh"
+if ! test -f "$HOME/posh-git-prompt.sh"; then
+  echo "'$HOME/posh-git-prompt.sh' is not available: https://github.com/lyze/posh-git-sh"
+  return 0
 fi
+
+# shellcheck disable=SC1090
+source "$HOME/posh-git-prompt.sh"
+
+# Thanks to:
+# https://github.com/demure/dotfiles/blob/master/subbash/prompt
+# https://brettterpstra.com/2009/11/17/my-new-favorite-bash-prompt/
+
+# Func to generate prompt after each command
+__prompt_command() {
+  local last_exit_code=$? # This needs to be first
+
+  local Reset='\[\e[0m\]'
+  local IRed='\[\e[0;91m\]'
+  local IGre='\[\e[0;92m\]'
+  local IYel='\[\e[0;93m\]'
+  local IBlu='\[\e[0;94m\]'
+  local IPur='\[\e[0;95m\]'
+  local ICya='\[\e[0;96m\]'
+  local IWhi='\[\e[0;97m\]'
+
+  PROMPT_DIRTRIM=2
+  PS1="${ICya}\u${IWhi}@${IPur}\h ${IGre}\t" # Username at host, time (24h)
+  PS1+=" ${IWhi}[${IYel}\w${IWhi}]${Reset}"  # Working directory, trimmed
+
+  # Git status
+  pge=$(__posh_git_echo)
+  if [ -n "$pge" ]; then
+    PS1+=" $pge"
+  fi
+
+  # Optional exit code
+  if [ "$last_exit_code" -eq 0 ]; then
+    PS1+=" ✅"
+  else
+    PS1+=" ❌ $IRed$last_exit_code$Reset"
+  fi
+
+  PS1+="\n${IWhi}\$${Reset} "
+}
+
+PROMPT_COMMAND=__prompt_command
+
+### https://github.com/lyze/posh-git-sh/issues/42
