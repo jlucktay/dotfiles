35f077f970d4ae73a32fa5c255d01bf7336a07ec
James Lucktaylor <jlucktay+github@gmail.com>
2020-01-13 09:39:01 +1100

    Leverage Bash's FUNCNAME.

--- .config/profile.d/407.func.template-go.sh
+++ .config/profile.d/407.func.template-go.sh
@@ -1,29 +1,28 @@
 function template_go() {
-  local func_name="template_go"
   local current_git_repo
   current_git_repo=$(get_git_root)
   local template_go_repo="$HOME/git/github.com/jlucktay/template-go/"
 
   if [ -z "$current_git_repo" ]; then
-    echo "$func_name: the current working directory is not inside a git repository"
+    echo "${FUNCNAME[0]}: the current working directory is not inside a git repository"
     return 1
   fi
 
   if [ ! -d "$template_go_repo" ]; then
-    echo "$func_name: the 'template-go' repo has not been checked out to '$template_go_repo'"
+    echo "${FUNCNAME[0]}: the 'template-go' repo has not been checked out to '$template_go_repo'"
     mkdir -pv "$template_go_repo"
     git clone https://github.com/jlucktay/template-go.git "$template_go_repo"
   fi
 
   if ! git --git-dir="${template_go_repo}.git" --work-tree="$template_go_repo" diff-index --quiet HEAD --; then
-    echo "$func_name: the 'template-go' repo at '$template_go_repo' is dirty"
+    echo "${FUNCNAME[0]}: the 'template-go' repo at '$template_go_repo' is dirty"
     return 1
   fi
 
   function usage() {
     \cat << HEREDOC
 
-    Usage: $func_name [--help|--confirm]
+    Usage: ${FUNCNAME[1]} [--help|--confirm]
 
     Will run 'rsync' on the git repo located at or above the current working directory, to bring in files from my
     template-go repository (located at '$template_go_repo').
@@ -48,7 +47,7 @@ HEREDOC
       shift
       ;;
     *) # unknown option
-      echo "$func_name: argument '$arg' unknown"
+      echo "${FUNCNAME[0]}: argument '$arg' unknown"
       usage
       return 1
       ;;
@@ -71,7 +70,7 @@ HEREDOC
     "$current_git_repo"
   )
 
-  echo "$func_name: running 'rsync' with the following arguments: ${rsync_args[*]}"
+  echo "${FUNCNAME[0]}: running 'rsync' with the following arguments: ${rsync_args[*]}"
 
   rsync "${rsync_args[@]}"
 }
