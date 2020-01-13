19aa66dd9bad1166a1a44054d41ce7a3d634ccbb
James Lucktaylor <jlucktay+github@gmail.com>
2020-01-13 16:59:29 +1100

    Handle deleted files.

--- bin/git-diff-whitespace-only-stage.sh
+++ bin/git-diff-whitespace-only-stage.sh
@@ -4,16 +4,19 @@ shopt -s nullglob globstar
 IFS=$'\n\t'
 
 while read -r diff_file; do
-  diff_without_whitespace=$(
-    git diff \
-      --ignore-all-space \
-      --ignore-blank-lines \
-      --unified=0 \
-      --word-diff-regex=[^[:space:]] \
-      "$diff_file"
-  )
-
-  if test -z "$diff_without_whitespace"; then
-    git add "$diff_file"
+  if stat "$diff_file" &> /dev/null; then
+    diff_without_whitespace=$(
+      git diff \
+        --ignore-all-space \
+        --ignore-blank-lines \
+        --unified=0 \
+        --word-diff-regex=[^[:space:]] \
+        "$diff_file"
+    )
+    if test -z "$diff_without_whitespace"; then
+      git add "$diff_file"
+    fi
+  else
+    git rm "$diff_file"
   fi
 done < <(git diff --name-only --relative)
