45ef95404795fcab76b88dcc13758ca75769dbcf
James Lucktaylor <jlucktay+github@gmail.com>
2020-01-30 18:17:53 +0000

    Reconfigure linter and script.

--- lint-bash.sh
+++ lint-bash.sh
@@ -11,19 +11,28 @@ shopt -s globstar nullglob
 script_dir="$(cd "$(dirname "${BASH_SOURCE[-1]}")" &> /dev/null && pwd)"
 
 # https://github.com/mvdan/sh
-find -x "$script_dir" -type f -iname "*sh" -print0 \
-  \! -name "git-completion.bash" \! -name "posh-git-prompt.sh" \
-  | xargs -0 shfmt -w -s -ln=bash -i=2 -bn -ci -sr
+find -x "$script_dir" -type f -iname "*sh" \
+  -not -name "git-completion.bash" \
+  -not -name "posh-git-prompt.sh" \
+  -not -path "*/.git/*" \
+  -print0 \
+  | xargs -0 -n 1 shfmt -w -s -ln=bash -i=2 -bn -ci -sr
 
 # https://github.com/anordal/shellharden
-find -x "$script_dir" -type f -iname "*sh" -print0 \
-  \! -name "git-completion.bash" \! -name "posh-git-prompt.sh" \
-  | xargs -0 shellharden --check --replace --
+find -x "$script_dir" -type f -iname "*sh" \
+  -not -name "git-completion.bash" \
+  -not -name "posh-git-prompt.sh" \
+  -not -path "*/.git/*" \
+  -print0 \
+  | xargs -0 -n 1 shellharden --check --replace --
 
 # https://github.com/koalaman/shellcheck
-find -x "$script_dir" -type f -iname "*sh" -print0 \
-  \! -name "git-completion.bash" \! -name "posh-git-prompt.sh" \
-  | xargs -0 shellcheck --enable=all --exclude=SC2250 --severity=style --shell=bash --
+find -x "$script_dir" -type f -iname "*sh" \
+  -not -name "git-completion.bash" \
+  -not -name "posh-git-prompt.sh" \
+  -not -path "*/.git/*" \
+  -print0 \
+  | xargs -0 -n 1 shellcheck --enable=all --exclude=SC2250 --severity=style --shell=bash --
 
 # Exclude: https://www.shellcheck.net/wiki/SC2250
 # Prefer putting braces around variable references even when not strictly required.
