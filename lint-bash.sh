8e9a6e6be9a2a8a7ba2b67ada54ac369d5ec8199
James Lucktaylor <jlucktay+github@gmail.com>
2020-03-02 15:39:48 +0000

    Run find once, not per linter. Rework shellharden approach.

--- lint-bash.sh
+++ lint-bash.sh
@@ -10,32 +10,42 @@ shopt -s globstar nullglob
 
 script_dir="$(cd "$(dirname "${BASH_SOURCE[-1]}")" &> /dev/null && pwd)"
 
+sh_files=()
+while IFS= read -r -d $'\0' file; do
+  sh_files+=("$file")
+done < <(
+  find -x "$script_dir" -type f -iname "*sh" \
+    -not -name "git-completion.bash" \
+    -not -name "posh-git-prompt.sh" \
+    -not -path "*/.git/*" \
+    -print0 \
+    | sort --zero-terminated
+)
+
 # https://github.com/mvdan/sh
-find -x "$script_dir" -type f -iname "*sh" \
-  -not -name "git-completion.bash" \
-  -not -name "posh-git-prompt.sh" \
-  -not -path "*/.git/*" \
-  -print0 \
-  | xargs -0 -n1 \
-    shfmt -w -s -ln=bash -i=2 -bn -ci -sr
+for sh_file in "${sh_files[@]}"; do
+  shfmt -w -s -ln=bash -i=2 -bn -ci -sr "$sh_file"
+done
 
 # https://github.com/anordal/shellharden
-find -x "$script_dir" -type f -iname "*sh" \
-  -not -name "git-completion.bash" \
-  -not -name "posh-git-prompt.sh" \
-  -not -path "*/.git/*" \
-  -print0 \
-  | xargs -0 -n1 -I{} \
-    shellharden --check {} --replace {}
+shellharden_count=0
+
+for sh_file in "${sh_files[@]}"; do
+  if ! shellharden --check -- "$sh_file" &> /dev/null; then
+    ((++shellharden_count))
+    echo "Applying shellharden replacements to '$sh_file'..."
+    shellharden --replace -- "$sh_file"
+  fi
+done
+
+if ((shellharden_count > 0)); then
+  exit "$shellharden_count"
+fi
 
 # https://github.com/koalaman/shellcheck
-find -x "$script_dir" -type f -iname "*sh" \
-  -not -name "git-completion.bash" \
-  -not -name "posh-git-prompt.sh" \
-  -not -path "*/.git/*" \
-  -print0 \
-  | xargs -0 -n1 \
-    shellcheck --enable=all --exclude=SC2250 --severity=style --shell=bash --external-sources --
+for sh_file in "${sh_files[@]}"; do
+  shellcheck --enable=all --exclude=SC2250 --severity=style --shell=bash --external-sources -- "$sh_file"
+done
 
 # Exclude: https://www.shellcheck.net/wiki/SC2250
 # Prefer putting braces around variable references even when not strictly required.
