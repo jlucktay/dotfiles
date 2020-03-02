0bd478e7ef6d816d2b0f235f7c2465dff5e481eb
James Lucktaylor <jlucktay+github@gmail.com>
2020-03-02 15:10:44 +0000

    Rework xargs invocation, and shellharden args.

--- lint-bash.sh
+++ lint-bash.sh
@@ -16,7 +16,8 @@ find -x "$script_dir" -type f -iname "*sh" \
   -not -name "posh-git-prompt.sh" \
   -not -path "*/.git/*" \
   -print0 \
-  | xargs -0 -n 1 shfmt -w -s -ln=bash -i=2 -bn -ci -sr
+  | xargs -0 -n1 \
+    shfmt -w -s -ln=bash -i=2 -bn -ci -sr
 
 # https://github.com/anordal/shellharden
 find -x "$script_dir" -type f -iname "*sh" \
@@ -24,7 +25,8 @@ find -x "$script_dir" -type f -iname "*sh" \
   -not -name "posh-git-prompt.sh" \
   -not -path "*/.git/*" \
   -print0 \
-  | xargs -0 -n 1 shellharden --check --replace --
+  | xargs -0 -n1 -I{} \
+    shellharden --check {} --replace {}
 
 # https://github.com/koalaman/shellcheck
 find -x "$script_dir" -type f -iname "*sh" \
@@ -32,7 +34,8 @@ find -x "$script_dir" -type f -iname "*sh" \
   -not -name "posh-git-prompt.sh" \
   -not -path "*/.git/*" \
   -print0 \
-  | xargs -0 -n 1 shellcheck --enable=all --exclude=SC2250 --severity=style --shell=bash --external-sources --
+  | xargs -0 -n1 \
+    shellcheck --enable=all --exclude=SC2250 --severity=style --shell=bash --external-sources --
 
 # Exclude: https://www.shellcheck.net/wiki/SC2250
 # Prefer putting braces around variable references even when not strictly required.
