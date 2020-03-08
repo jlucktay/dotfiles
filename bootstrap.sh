bf8d8c0a7a2473db66474deb3ec54372f9d706fd
James Lucktaylor <jlucktay+github@gmail.com>
2020-03-08 19:12:50 +0000

    Bootstrap chezmoi into /usr/local/bin.

--- bootstrap.sh
+++ bootstrap.sh
@@ -3,6 +3,6 @@ set -euo pipefail
 shopt -s globstar nullglob
 
 cd "$HOME"
-curl -sfL https://git.io/chezmoi | sh
+curl --fail --location --silent https://git.io/chezmoi | BINDIR=/usr/local/bin sudo --preserve-env=BINDIR sh
 PATH=$HOME/bin:$PATH
 chezmoi init --apply --verbose https://github.com/jlucktay/dotfiles.git
