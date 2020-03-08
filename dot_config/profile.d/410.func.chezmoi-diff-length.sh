d827993dadf69f1a40e4073c9c771e1bce3f6775
James Lucktaylor <jlucktay+github@gmail.com>
2020-03-08 06:25:29 +0000

    Add chezmoi diff length func.

---
+++ dot_config/profile.d/410.func.chezmoi-diff-length.sh
@@ -0,0 +1,14 @@
+if hash chezmoi &> /dev/null; then
+  function cdl() {
+    local result
+    result=$(chezmoi diff | wc -l)
+
+    if [ "$result" -gt 255 ]; then
+      result=255
+    fi
+
+    return "$result"
+  }
+
+  export -f cdl
+fi
