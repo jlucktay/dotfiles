b4704771ca684084d0eb39d3a018020df9ee2226
James Lucktaylor <jlucktay+github@gmail.com>
2020-03-16 14:06:06 +0000

    Add new script for discovering Go tests.

---
+++ bin/executable_go-list-tests.sh
@@ -0,0 +1,16 @@
+#!/usr/bin/env bash
+set -euo pipefail
+shopt -s globstar nullglob
+IFS=$'\n\t'
+
+for package in $(go list ./...); do
+  test_name_pattern="^(Bench|Example|Test)"
+  go_test_output=$(go test -list="$test_name_pattern" "$package")
+
+  if [[ $go_test_output =~ ^\? ]]; then
+    continue
+  fi
+
+  awk -v PKG="$package" -v TNP="$test_name_pattern" \
+    '$1 ~ TNP {print PKG, ">", $0}' <<< "$go_test_output"
+done
