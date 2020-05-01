1bfecdb4ef589ef9a97b43a6558a7886f511cbee
James Lucktaylor <jlucktay+github@gmail.com>
2020-05-01 14:04:15 +0100

    Add 'gp' function for git push with WIP check.

---
+++ dot_config/profile.d/413.func.gp.sh
@@ -0,0 +1,14 @@
+if hash git &> /dev/null && hash rg &> /dev/null && hash awk &> /dev/null && hash xargs &> /dev/null; then
+  function gp() {
+    git_wip=$(git log --oneline origin/master.. | rg --fixed-strings "WIP: " | awk '{ print $1 }' | xargs git show)
+
+    if [ -n "$git_wip" ]; then
+      echo "WIP!"
+      return 1
+    fi
+
+    git push
+  }
+
+  export -f gp
+fi
