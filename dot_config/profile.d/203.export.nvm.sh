167b9363c3527e42ca5f64e69dc3c77e90dffea2
James Lucktaylor <jlucktay+github@gmail.com>
2020-07-05 04:32:48 +0100

    Set up nvm.

---
+++ dot_config/profile.d/203.export.nvm.sh
@@ -0,0 +1,7 @@
+export NVM_DIR="$HOME/.nvm"
+
+# This loads nvm
+[ -s "/usr/local/opt/nvm/nvm.sh" ] && . "/usr/local/opt/nvm/nvm.sh"
+
+# This loads nvm bash_completion
+[ -s "/usr/local/opt/nvm/etc/bash_completion.d/nvm" ] && . "/usr/local/opt/nvm/etc/bash_completion.d/nvm"
