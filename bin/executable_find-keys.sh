3984b01d0c1a95c475f2d330d1d4575badd9ec04
James Lucktaylor <jlucktay+github@gmail.com>
2020-07-30 10:05:49 +0100

    feat(bin): dude, where's my key(s)?

---
+++ bin/executable_find-keys.sh
@@ -0,0 +1,9 @@
+#!/usr/bin/env bash
+set -euo pipefail
+
+while read -r j; do
+  if has_key=$(jq --exit-status '.private_key' "$j" 2> /dev/null); then
+    has_key_trimmed=$(cut -c 1-100 <<< "$has_key")
+    printf "%s\n%-50s\n\n" "$j" "$has_key_trimmed"
+  fi
+done < <(fd -e json)
