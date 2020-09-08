cce22128aa34519ba86bec05ecea671e274c2c0d
James Lucktaylor <jlucktay+github@gmail.com>
2020-09-08 12:09:19 +0100

    fix(function/awsregions): wire up new read-only credentials

--- dot_config/profile.d/405.func.awsregions.sh
+++ dot_config/profile.d/405.func.awsregions.sh
@@ -1,6 +1,7 @@
 if hash aws &> /dev/null; then
   function awsregions() {
-    aws ec2 describe-regions --region=eu-west-1 | jq --raw-output '.Regions[].RegionName' | gsort --ignore-case
+    aws --profile awsregions ec2 describe-regions \
+      | jq --raw-output '.Regions[].RegionName' | gsort --ignore-case
   }
 
   export -f awsregions
