59fa28bb5dc9e771f335b9083bdd1822544ad7a5
James Lucktaylor <jlucktay+github@gmail.com>
2021-01-23 19:50:08 +0000

    feat(func/golint_enable_all): fix parsing, and add disable flag to override (ugh exhaustivestruct)

--- dot_config/profile.d/414.func.golint_enable_all.sh
+++ dot_config/profile.d/414.func.golint_enable_all.sh
@@ -1,19 +1,28 @@
 # https://github.com/golangci/golangci-lint
 if hash golangci-lint &> /dev/null; then
   function golint_enable_all() {
+    local -a do_not_enable
+
+    for arg in "$@"; do
+      if [[ $arg =~ "--disable="* ]]; then
+        do_not_enable+=("${arg/--disable=/}")
+        shift
+      fi
+    done
+
+    # TODO: remove 'effected' once this PR merges/releases: https://github.com/golangci/golangci-lint/pull/1663
     golangci-lint linters \
-      | awk 'BEGIN { FS = "[ :]" } ; $1 != "Enabled" && $1 != "Disabled" && length($1) > 0 { print $1 }' \
+      | awk '$0 ~ /:/ && $1 !~ /^((En|Dis)abled|effected)$/ { gsub(/:/, "", $1); print $1 }' \
       | {
         local -a enable
 
         while IFS=$'\n' read -r linter; do
-          enable+=("$linter")
+          if [[ ! " ${do_not_enable[*]} " =~ \ ${linter}\  ]]; then
+            enable+=("$linter")
+          fi
         done
 
-        linters_joined="$(
-          IFS=','
-          echo "${enable[*]}"
-        )"
+        IFS=',' linters_joined="${enable[*]}"
 
         golangci-lint run --disable-all --enable="$linters_joined" "$@"
       }
