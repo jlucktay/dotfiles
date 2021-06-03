191b7c58976c00c41709aa48b017bb0f00df2ddf
James Lucktaylor <jlucktay+github@gmail.com>
2021-06-03 16:10:48 +0100

    feat(bootstrap): add an optional purge flag

--- README.md
+++ README.md
@@ -9,3 +9,10 @@ Run this to bootstrap a new environment:
 ``` shell
 curl --fail --location --silent https://git.io/jlucktay-dotfiles | bash
 ```
+
+Set `CHEZMOI_PURGE=1` on the script execution to remove Chezmoi's source/config directories after initialising and
+applying:
+
+``` shell
+curl --fail --location --silent https://git.io/jlucktay-dotfiles | CHEZMOI_PURGE=1 bash
+```
--- bootstrap.sh
+++ bootstrap.sh
@@ -3,4 +3,17 @@ set -euo pipefail
 
 curl --fail --location --silent https://git.io/chezmoi | BINDIR="$HOME/bin" sh
 sudo mv "$HOME"/bin/chezmoi /usr/local/bin
-chezmoi init --apply https://github.com/jlucktay/dotfiles.git
+
+chezmoi_args=(
+  init
+  jlucktay
+  --apply
+  --depth 1
+  --force
+)
+
+if [ "${CHEZMOI_PURGE:-0}" == 1 ]; then
+  chezmoi_args+=(--purge)
+fi
+
+chezmoi "${chezmoi_args[@]}"
