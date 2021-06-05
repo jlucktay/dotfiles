e1e05869d9e6e59293321c5cd866f11cc38d5c22
James Lucktaylor <jlucktay+github@gmail.com>
2021-06-05 16:57:31 +0100

    feat(bash/profile): set no-op for cheatsheet on Pis

--- dot_config/profile.d/900.cheat-sheet.sh
+++ dot_config/profile.d/900.cheat-sheet.sh
@@ -1,5 +1,8 @@
 # Cheat sheet
-if test -r "$HOME/bash-cheat-sheet.txt"; then
+if [[ $(hostname) =~ ^rpi[28]-[1..3]$ ]]; then
+  # No-op on my home Pi cluster
+  :
+elif test -r "$HOME/bash-cheat-sheet.txt"; then
   \cat "$HOME/bash-cheat-sheet.txt"
 else
   echo "'$HOME/bash-cheat-sheet.txt' not available"
