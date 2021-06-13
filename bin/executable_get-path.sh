e921ee7178e94a6bd8b02b6867e12691d930abe8
James Lucktaylor <jlucktay+github@gmail.com>
2021-06-13 14:23:13 +0100

    refactor(bin/get-path): simplify

--- bin/executable_get-path.sh
+++ bin/executable_get-path.sh
@@ -5,21 +5,11 @@ background_white="$(tput setab 7)"
 foreground_black="$(tput setaf 0)"
 reset_colours="$(tput sgr0)"
 
-echo "$background_white${foreground_black}PATH unsorted:$reset_colours"
+echo "$background_white${foreground_black}\$PATH unsorted:$reset_colours"
 
-while IFS=':' read -ra split_path; do
-  # Split off sorted $PATH array for later output
-  IFS=$'\n' mapfile -t sorted_path < <(sort -f <<< "${split_path[*]}")
-
-  # Show unsorted $PATH now
-  for i in "${split_path[@]}"; do
-    echo "$i"
-  done
-done <<< "$PATH"
+tr ':' '\n' <<< "$PATH"
 
 echo
-echo "$background_white${foreground_black}PATH sorted:$reset_colours"
+echo "$background_white${foreground_black}\$PATH sorted:$reset_colours"
 
-for i in ${sorted_path[*]}; do
-  echo "$i"
-done
+tr ':' '\n' <<< "$PATH" | sort -f
