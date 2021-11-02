49b2101f6841dac5659be36c33dda534d0edaebb
James Lucktaylor <jlucktay+github@gmail.com>
2021-11-02 10:03:33 +0000

    fix(func/interview): polish off lower-casing and joining

--- dot_config/profile.d/415.func.interview.sh
+++ dot_config/profile.d/415.func.interview.sh
@@ -6,7 +6,7 @@ function interview() {
   if command -v gtr &> /dev/null; then tr_binary=gtr; fi
 
   # Equivalent of macOS "open" for all the other modern *nixes.
-  if ! command -v open; then
+  if ! command -v open &> /dev/null; then
     function open() {
       for arg in "$@"; do
         setsid nohup xdg-open "$arg" &> /dev/null
@@ -25,14 +25,25 @@ function interview() {
 
   # Take the '$@' array of arguments, and 'tr' each arg to filter down to lowercase alphanumeric characters only.
   for arg in "$@"; do
-    local tr_result
-    tr_result=$($tr_binary -cd "[:alnum:]" <<< "$arg")
-    filtered+=("$tr_result")
+    local tr_alnum tr_lower
+    tr_alnum=$($tr_binary -cd "[:alnum:]" <<< "$arg")
+    tr_lower=$($tr_binary '[:upper:]' '[:lower:]' <<< "$tr_alnum")
+    filtered+=("$tr_lower")
   done
 
+  # Set up a temporary function.
+  function join_by {
+    local IFS="$1"
+    shift
+    echo "$*"
+  }
+
   # Join the filtered down list with hyphens, to derive the target filename.
-  IFS='-' local filtered_joined="${filtered[*]}"
-  local target_filename=$HOME/Documents/Notes/Candidates/$filtered_joined.txt
+  local target_filename
+  target_filename=$HOME/Documents/Notes/Candidates/$(join_by - "${filtered[@]}").txt
+
+  # Clear out the temporary function.
+  unset join_by
 
   # Stat the target filename;  if it does not exist, use 'touch' to create.
   if ! $stat_binary "$target_filename" &> /dev/null; then
