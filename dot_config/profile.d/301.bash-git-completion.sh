20973aaf9a9be20c607190f296bdf3c85545492b
James Lucktaylor <jlucktay+github@gmail.com>
2022-03-17 14:54:22 +0000

    feat(tfenv): add dynamic tfenv/terraform completion

--- dot_config/profile.d/301.bash-git-completion.sh
+++ dot_config/profile.d/301.bash-git-completion.sh
@@ -14,3 +14,15 @@ fi
 if command -v kubectl &> /dev/null; then
   complete -F __start_kubectl k
 fi
+
+# tfenv and Terraform
+tfenv_prefix=$(brew --prefix tfenv)
+
+tfenv_list_exit=0
+tfenv list &> /dev/null || tfenv_list_exit=$?
+
+if [[ -d $tfenv_prefix ]] && [[ $tfenv_list_exit -eq 0 ]] && [[ -r "$tfenv_prefix"/version ]] \
+  && command -v terraform &> /dev/null; then
+
+  complete -C "$tfenv_prefix"/versions/"$(< "$tfenv_prefix"/version)"/terraform terraform
+fi
