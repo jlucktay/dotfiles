dfa849f960777b5937953a871457e824f0d29d1d
James Lucktaylor <jlucktay@users.noreply.github.com>
2022-05-12 14:11:45 +0100

    Update run_list.sh

--- lists/run_list.sh
+++ lists/run_list.sh
@@ -22,7 +22,7 @@ function process_list() {
   cmd_name=$(awk '{ print $1 }' <<< "$1")
 
   if ! hash "$cmd_name" &> /dev/null; then
-    echo "${BASH_SOURCE[-1]} > ${FUNCNAME[0]}: command '$cmd_name' not found; aborting"
+    echo "${BASH_SOURCE[${#BASH_SOURCE[@]}-1]} > ${FUNCNAME[0]}: command '$cmd_name' not found; aborting"
     return
   fi
 
