1ec266134f11fee2c5229fbac402e03da03c2f15
James Lucktaylor <jlucktay+github@gmail.com>
2022-06-13 04:20:10 +0100

    style(lists): autoformat, add braces, etc

--- lists/run_list.sh
+++ lists/run_list.sh
@@ -3,21 +3,21 @@ set -euo pipefail
 
 # Bash before 4.4 (like the default one on Macs these days) doesn't have this option:
 # https://news.ycombinator.com/item?id=24738359
-shopt -s inherit_errexit 2>/dev/null || true
+shopt -s inherit_errexit 2> /dev/null || true
 
 # Bash before 4.2 (like the default one on Macs these days) doesn't support negative subscripts:
 # https://stackoverflow.com/a/61345169/380599
-script_name=$(basename "${BASH_SOURCE[${#BASH_SOURCE[@]}-1]}")
+script_name=$(basename "${BASH_SOURCE[${#BASH_SOURCE[@]} - 1]}")
 
 if ! chezmoi_source="$(chezmoi data | jq --exit-status --raw-output '.chezmoi.sourceDir')"; then
-  echo "$script_name: couldn't fetch source directory from 'chezmoi data'"
+  echo "${script_name}: couldn't fetch source directory from 'chezmoi data'"
   exit 1
 fi
 
 if ! command -v realpath; then
   # https://stackoverflow.com/a/3572105/380599
   function realpath() {
-    [[ $1 = /* ]] && echo "$1" || echo "$PWD/${1#./}"
+    [[ $1 == /* ]] && echo "$1" || echo "${PWD}/${1#./}"
   }
 fi
 
@@ -28,17 +28,17 @@ function process_list() {
   # Parse out the command name and make sure it is available
   cmd_name=$(awk '{ print $1 }' <<< "$1")
 
-  if ! hash "$cmd_name" &> /dev/null; then
-    echo "${BASH_SOURCE[${#BASH_SOURCE[@]}-1]} > ${FUNCNAME[0]}: command '$cmd_name' not found; aborting"
+  if ! hash "${cmd_name}" &> /dev/null; then
+    echo "${BASH_SOURCE[${#BASH_SOURCE[@]} - 1]} > ${FUNCNAME[0]}: command '${cmd_name}' not found; aborting"
     return
   fi
 
   # Print command and first argument
-  printf "%s: [%s] " "$script_name" "$(gdn)"
+  printf "%s: [%s] " "${script_name}" "$(gdn)"
   awk '{ print $1, $2 }' <<< "$1"
 
   # If the command is 'brew' then make sure we're up to date before kicking off the lists
-  if [ "$cmd_name" == "brew" ]; then
+  if [[ ${cmd_name} == "brew" ]]; then
     brew update
   fi
 
@@ -46,24 +46,24 @@ function process_list() {
     : # No-op; skipping errors
   fi
 
-  sort --ignore-case <<< "$cmd_output" > "$(realpath "$chezmoi_source/lists/text/list.$2.txt")"
+  sort --ignore-case <<< "${cmd_output}" > "$(realpath "${chezmoi_source}/lists/text/list.$2.txt")"
 }
 
 # Git repos I have checked out locally
-git_list_cmd="fd '^\.git$' \"$HOME\" \
+git_list_cmd="fd '^\.git$' \"${HOME}\" \
   --hidden --type d \
   --exec git -C '{//}' rev-parse --show-toplevel \
   | grep -v \"\/Library\/\" "
 
-process_list "$git_list_cmd" "git"
+process_list "${git_list_cmd}" "git"
 
 # Go binaries
 # https://stackoverflow.com/a/13864829/380599
-if [ -z ${GOPATH+x} ]; then
+if [[ -z ${GOPATH+x} ]]; then
   echo "GOPATH is unset, skipping"
 else
-  go_bin_list_cmd="find $GOPATH/bin -type f"
-  process_list "$go_bin_list_cmd" "go.bin"
+  go_bin_list_cmd="find ${GOPATH}/bin -type f"
+  process_list "${go_bin_list_cmd}" "go.bin"
 fi
 
 # Homebrew
@@ -76,26 +76,26 @@ npm_list_cmd="npm list --depth=0 --global --parseable"
 
 ### If NVM is installed, use it to iterate across all available versions
 # shellcheck disable=SC1091
-if [ -s "$(brew --prefix nvm)/nvm.sh" ] && . "$(brew --prefix nvm)/nvm.sh" &> /dev/null; then
+if [[ -s "$(brew --prefix nvm)/nvm.sh" ]] && . "$(brew --prefix nvm)/nvm.sh" &> /dev/null; then
   mapfile -t nvm_versions < <(nvm ls --no-alias --no-colors | cut -c3-15 | tr -d ' ')
 
   for nvm_version in "${nvm_versions[@]}"; do
-    nvm use "$nvm_version" &> /dev/null
-    echo "Node: $nvm_version / npm: $(npm --version)"
-    process_list "$npm_list_cmd" "npm.$nvm_version"
+    nvm use "${nvm_version}" &> /dev/null
+    echo "Node: ${nvm_version} / npm: $(npm --version)"
+    process_list "${npm_list_cmd}" "npm.${nvm_version}"
   done
 else
-  process_list "$npm_list_cmd" "npm"
+  process_list "${npm_list_cmd}" "npm"
 fi
 
 # VSCode extensions
 vscode_list_cmd="code --list-extensions"
-process_list "$vscode_list_cmd" "vscode"
+process_list "${vscode_list_cmd}" "vscode"
 
 # Python packages
 pip_list_cmd="python3 -m pip list --format freeze"
-process_list "$pip_list_cmd" "pip3"
+process_list "${pip_list_cmd}" "pip3"
 
 # Ruby gems
 gem_list_cmd="gem list --no-verbose"
-process_list "$gem_list_cmd" "gem"
+process_list "${gem_list_cmd}" "gem"
