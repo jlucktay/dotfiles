75e40a6cc7101b09979bbdb57b974a6c56642c9c
James Lucktaylor <jlucktay+github@gmail.com>
2022-06-15 14:55:50 +0100

    refactor(lint-bash.sh): redo

--- lint-bash.sh
+++ lint-bash.sh
@@ -1,36 +1,39 @@
 #!/usr/bin/env bash
 set -euo pipefail
 
+### NOTES
 # Linters called by this script are (where possible) configured to follow Google's shell style guide:
 # https://google.github.io/styleguide/shell.xml
 
 script_dir="$(cd "$(dirname "${BASH_SOURCE[-1]}")" &> /dev/null && pwd)"
 
-sh_files=()
-dirty_files=()
-
 # When calling 'find', some scripts that aren't mine are excluded.
-while IFS= read -r -d $'\0' file; do
-  sh_files+=("$file")
-done < <(
-  find -x "$script_dir" -type f -iname "*sh" \
+find_scripts=$(
+  find -x "${script_dir}" -type f -iname "*sh" \
     -not -name "git-completion.bash" \
     -not -name "posh-git-prompt.sh" \
-    -not -path "*/.git/*" \
-    -print0 \
-    | sort --zero-terminated
+    -not -path "*/.git/*"
 )
 
-# https://github.com/koalaman/shellcheck
-# Exclude: https://www.shellcheck.net/wiki/SC2250
-# Prefer putting braces around variable references even when not strictly required.
-for sh_file in "${sh_files[@]}"; do
-  shellcheck --enable=all --exclude=SC2250 --severity=style --shell=bash --external-sources -- "$sh_file"
-done
+mapfile -t script_files <<< "${find_scripts}"
 
-# https://github.com/mvdan/sh
-for sh_file in "${sh_files[@]}"; do
-  if ! shfmt -w -d -s -ln=bash -i=2 -bn -ci -sr "$sh_file" &> /dev/null; then
-    dirty_files+=("$sh_file")
-  fi
+# Roll up flags for 'shfmt', as there are quite a few.
+shfmt_flags=(
+  "--binary-next-line"
+  "--case-indent"
+  "--indent=2"
+  "--simplify"
+  "--space-redirects"
+  "--write"
+)
+
+for script_file in "${script_files[@]}"; do
+  # https://github.com/mvdan/sh
+  shfmt "${shfmt_flags[@]}" -- "${script_file}"
+
+  # https://github.com/anordal/shellharden
+  shellharden --check --replace -- "${script_file}"
+
+  # https://github.com/koalaman/shellcheck
+  shellcheck --enable=all --external-sources --severity=style --shell=bash -- "${script_file}"
 done
