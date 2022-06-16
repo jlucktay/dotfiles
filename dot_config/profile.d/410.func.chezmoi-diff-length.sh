7561efefa714fb90a88c82a21223632567ccd82c
James Lucktaylor <jlucktay+github@gmail.com>
2022-06-16 11:32:58 +0100

    feat(func/cdes): finish up

--- dot_config/profile.d/410.func.chezmoi-diff-length.sh
+++ dot_config/profile.d/410.func.chezmoi-diff-length.sh
@@ -15,12 +15,37 @@ if command -v chezmoi &> /dev/null; then
 
   export -f cdl
 
-  function cdes() {
-    local chezmoi_diff
-    chezmoi_diff=$(chezmoi diff --exclude=scripts)
+  if command -v bat &> /dev/null; then
+    function cdes() {
+      # If there are multiple different files, show the diff of the first file only.
+      local first_split=0
+      local -a cat_input
+      local chezmoi_diff
+      chezmoi_diff=$(chezmoi diff --exclude=scripts)
 
-    echo "$chezmoi_diff"
-  }
+      while IFS= read -r line; do
+        IFS=$'\n' cat_input+=("$line")
 
-  export -f cdes
+        if [[ $line =~ ^diff[[:space:]]--git[[:space:]]a/ ]]; then
+          if [[ $first_split -eq 0 ]]; then
+            local file_name=${line##diff --git a/}
+            file_name=${file_name% b/*}
+
+            ((first_split++))
+            continue
+          fi
+
+          break
+        fi
+      done <<< "$chezmoi_diff"
+
+      if [[ ${#cat_input[@]} -le 2 ]]; then
+        return 0
+      fi
+
+      bat --file-name "$file_name" --language=diff <<< "${cat_input[*]}"
+    }
+
+    export -f cdes
+  fi
 fi
