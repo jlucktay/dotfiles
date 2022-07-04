cf7a55a55e230f02d9dfd11ecd8cf3f487d4b553
James Lucktaylor <jlucktay+github@gmail.com>
2022-07-04 17:09:28 +0100

    feat(bash): bind (shift+)TAB to (reverse) menu-complete, and magic-space for history

--- dot_config/profile.d/300.bash-shopt-stty.sh
+++ dot_config/profile.d/300.bash-shopt-stty.sh
@@ -1,7 +1,17 @@
-### Bash setup
+# Bash setup
 
-# https://www.linuxjournal.com/content/globstar-new-bash-globbing-option
+## https://www.linuxjournal.com/content/globstar-new-bash-globbing-option
 shopt -s globstar
 
-# https://unix.stackexchange.com/questions/12107/how-to-unfreeze-after-accidentally-pressing-ctrl-s-in-a-terminal
+## https://unix.stackexchange.com/questions/12107/how-to-unfreeze-after-accidentally-pressing-ctrl-s-in-a-terminal
 stty -ixon -ixoff
+
+## Bind keys to shell operations/functions
+
+### Perform history expansion on the current line and insert a space
+bind 'Space: magic-space'
+
+### Replaces the word to be completed with a single match from the list of possible completions.
+### Repeated execution of menu-complete steps through the list of possible completions, inserting each match in turn.
+bind 'TAB: menu-complete'
+bind '"\e[Z": menu-complete-backward'
