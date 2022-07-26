971e74126530e155fd7ad571fc918539a0613263
James Lucktaylor <jlucktay+github@gmail.com>
2022-07-26 09:35:56 +0100

    fix(bootstrap): no can do boss

--- bootstrap.sh
+++ bootstrap.sh
@@ -4,8 +4,6 @@ set -euo pipefail
 if ! command -v chezmoi &> /dev/null; then
   curl --fail --location --silent https://git.io/chezmoi | BINDIR="$HOME/bin" sh
   sudo mv "$HOME"/bin/chezmoi /usr/local/bin
-else
-  sudo chezmoi upgrade --executable=/usr/local/bin/chezmoi
 fi
 
 chezmoi_args=(
