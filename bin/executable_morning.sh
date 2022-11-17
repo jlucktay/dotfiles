fea5be4b40a4ad93a76281e314f2b44c463443dd
James Lucktaylor <jlucktay+github@gmail.com>
2022-11-17 09:17:14 +0000

    feat(bin): morning counterpart to EOD script

---
+++ bin/executable_morning.sh
@@ -0,0 +1,18 @@
+#!/usr/bin/env bash
+set -euo pipefail
+
+# Boilerplate to bring in library script(s).
+script_directory="$(cd "$(dirname "${BASH_SOURCE[-1]}")" &> /dev/null && pwd)"
+readonly script_directory
+
+for lib in "$script_directory"/lib/*.sh; do
+  # shellcheck disable=SC1090
+  source "$lib"
+done
+
+# The real Dark Souls starts here.
+tool_check limactl topgrade
+
+limactl start
+
+topgrade
