32fc24e3bc6b934667e9890ed6014c1fcbc7d0ca
James Lucktaylor <jlucktay+github@gmail.com>
2023-03-29 10:27:07 +0100

    feat(stashcheck): only output repos with stashes

--- dot_config/profile.d/403.func.git-stashcheck.sh
+++ dot_config/profile.d/403.func.git-stashcheck.sh
@@ -3,16 +3,24 @@ if command -v git &> /dev/null; then
     local -
     set -o pipefail
 
-    find "$HOME/git" -type d -name ".git" -print0 | while IFS= read -d '' -r git; do
-      git_stash_list=$(GIT_DIR="$git" git stash list)
+    echo "[$(gdn || true)] ${FUNCNAME[0]}: finding git repos..."
+
+    git_repos=$(find "$HOME/git" -type d -name ".git")
+    mapfile -t repos <<< "$git_repos"
+
+    echo "[$(gdn || true)] ${FUNCNAME[0]}: found ${#repos[@]} git repo(s)."
+
+    for ((i = 0; i < ${#repos[@]}; i++)); do
+      git_stash_list=$(GIT_DIR="${repos[$i]}" git stash list)
       mapfile -t stash <<< "$git_stash_list"
 
-      if ((${#stash[@]} > 0)); then
-        realpath "$git/.."
+      if ((${#stash[@]} >= 1)) && [[ -n ${stash[0]} ]]; then
+        realpath "${repos[$i]}/.."
+
+        for stash_line in "${stash[@]}"; do
+          printf "\t%s\n" "$stash_line"
+        done
       fi
-      for stash_line in "${stash[@]}"; do
-        printf "\t%s\n" "$stash_line"
-      done
     done
   }
 fi
