903f7b416957685eb72159f42a0efebc8074f16d
James Lucktaylor <jlucktay+github@gmail.com>
2023-04-06 15:13:05 +0100

    feat(git): remove func and add config around it instead

--- dot_config/profile.d/413.func.gp.sh
+++ dot_config/profile.d/413.func.gp.sh
@@ -18,13 +18,4 @@ if command -v git &> /dev/null && command -v rg &> /dev/null && command -v awk &
   }
 
   export -f gp
-
-  function gpsu() {
-    local git_sr_head
-    git_sr_head=$(git symbolic-ref --short HEAD)
-
-    gp --set-upstream origin "$git_sr_head" "$@"
-  }
-
-  export -f gpsu
 fi
