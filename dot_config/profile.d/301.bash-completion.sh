f6cb4f369812b92371edd8c1a6a4d259c8c9820b
James Lucktaylor <jlucktay+github@gmail.com>
2023-06-13 14:57:56 +0100

    feat(.config/profile): add tabtab completion, for pnpm

--- dot_config/profile.d/301.bash-completion.sh
+++ dot_config/profile.d/301.bash-completion.sh
@@ -1,13 +1,19 @@
 # Bash completion
-if test -r "${package_manager_prefix:?}/etc/profile.d/bash_completion.sh"; then
+if [[ -r "${package_manager_prefix:?}/etc/profile.d/bash_completion.sh" ]]; then
   # export BASH_COMPLETION_COMPAT_DIR="/usr/local/etc/bash_completion.d"
 
   # shellcheck disable=SC1091
   source "${package_manager_prefix:?}/etc/profile.d/bash_completion.sh"
 fi
 
+# Completion via tabtab
+if [[ -r "$HOME/.config/tabtab/bash/__tabtab.bash" ]]; then
+  # shellcheck disable=SC1091
+  source "$HOME/.config/tabtab/bash/__tabtab.bash"
+fi
+
 # Git completion
-if command -v git &> /dev/null && test -r "$HOME/git-completion.bash"; then
+if command -v git &> /dev/null && [[ -r "$HOME/git-completion.bash" ]]; then
   # shellcheck disable=SC1091
   source "$HOME/git-completion.bash"
 fi
