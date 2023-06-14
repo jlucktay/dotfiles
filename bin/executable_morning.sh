48f3eed2fa35ada351ecbaa0312eca1167f1a118
James Lucktaylor <jlucktay+github@gmail.com>
2023-06-14 06:02:37 +0100

    feat(bin/morning): drill down to Cubs affiliates only

--- bin/executable_morning.sh
+++ bin/executable_morning.sh
@@ -14,10 +14,14 @@ done
 dslog "start"
 trap 'dslog "finish"' 0
 
+date_today="$(date '+%Y')/"
+date_today+="$(date '+%m')/"
+date_today+="$(date '+%d')"
+
 declare -a morning_commands=(
-  # Check the MLB.TV and MiLB.TV schedules.
+  # Check the schedules for MLB.TV and MiLB.TV (Cubs affiliates).
   "open 'https://www.mlb.com/live-stream-games'"
-  "open 'https://www.milb.com/live-stream-games'"
+  "open 'https://www.milb.com/live-stream-games/$date_today/all/all/cubs'"
 
   # Refresh GitHub/OVO SSO for the day.
   "open 'https://github.com/orgs/ovotech/teams/ovodevex/members'"
