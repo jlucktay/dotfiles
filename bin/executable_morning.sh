75e0195d81b201b2a8d7b5c97eb117a2d49ceff8
James Lucktaylor <jlucktay+github@gmail.com>
2023-11-02 15:11:38 +0000

    feat(bin/morning.sh): account for the offseason

--- bin/executable_morning.sh
+++ bin/executable_morning.sh
@@ -18,11 +18,27 @@ date_today="$(date '+%Y')/"
 date_today+="$(date '+%m')/"
 date_today+="$(date '+%d')"
 
-declare -a morning_commands=(
-  # Check the schedules for MLB.TV and MiLB.TV (Cubs affiliates).
-  "open 'https://www.mlb.com/live-stream-games'"
-  "open 'https://www.milb.com/live-stream-games/$date_today/all/all/cubs'"
+#
+mlb_next_season_start='2024-03-28'
+#
 
+declare -a morning_commands=()
+
+if command -v gdate &> /dev/null; then
+  mlb_epoch=$(gdate --date="$mlb_next_season_start" +%s)
+  today_epoch=$(gdate --date="$date_today" +%s)
+  mlb_season_has_started=$(("$today_epoch" - "$mlb_epoch"))
+
+  if [[ $mlb_season_has_started -ge 0 ]]; then
+    morning_commands+=(
+      # Check the schedules for MLB.TV and MiLB.TV (Cubs affiliates).
+      "open 'https://www.mlb.com/live-stream-games'"
+      "open 'https://www.milb.com/live-stream-games/$date_today/all/all/cubs'"
+    )
+  fi
+fi
+
+morning_commands+=(
   # Refresh GitHub/OVO SSO for the day.
   "open 'https://github.com/orgs/ovotech/teams/ovodevex/members'"
 
