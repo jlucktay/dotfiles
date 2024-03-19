e41ea3b2e91d11d7876ca45c4a1aa6fe273a9168
James Lucktaylor <jlucktay+github@gmail.com>
2024-03-19 19:47:44 +0000

    feat(gcr-usage.sh): add new script

---
+++ bin/executable_gcr-usage.sh
@@ -0,0 +1,14 @@
+#!/usr/bin/env bash
+set -euo pipefail
+
+tmp_orgs=$(gcloud organizations list --format=json | jq --raw-output '.[].name')
+mapfile -t google_cloud_orgs <<< "$tmp_orgs"
+
+for org in "${google_cloud_orgs[@]}"; do
+  split_org=$(cut -d'/' -f2 <<< "$org")
+  echo >&2 "Org (split): '$split_org'"
+
+  gcloud container images list-gcr-usage --organization="$split_org" --format=json
+
+  echo >&2
+done
