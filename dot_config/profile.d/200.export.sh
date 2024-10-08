90d6c06e9ef575521dc6cfd597e9b4440ecab1d6
James Lucktaylor <jlucktay+github@gmail.com>
2024-10-08 14:04:43 +0100

    feat(profile): export Cloudflare CA for AWS CLI vs ZTNA

--- dot_config/profile.d/200.export.sh
+++ dot_config/profile.d/200.export.sh
@@ -109,3 +109,9 @@ fi
 if command -v bat &> /dev/null; then
   export MANPAGER="sh -c 'col -bx | bat -l man -p'"
 fi
+
+# AWS CLI MITM
+# https://ovotech.atlassian.net/wiki/spaces/OTKB/pages/4596302401/Certificate+Error+When+Connecting+to+Resources+via+ZTNA
+if command -v aws &> /dev/null; then
+  export AWS_CA_BUNDLE="$HOME/ztna-mitm/Cloudflare_CA.pem"
+fi
