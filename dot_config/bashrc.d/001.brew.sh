d5d2178c559b59d67beccb727a987486430f4e79
James Lucktaylor <jlucktay+github@gmail.com>
2024-10-13 23:27:01 +0100

    feat(bashrc/brew): account for Linuxbrew as well

--- dot_config/bashrc.d/001.brew.sh
+++ dot_config/bashrc.d/001.brew.sh
@@ -1,7 +1,14 @@
-# If Homebrew is installed, do the thing.
-if test -x /opt/homebrew/bin/brew; then
-  eval "$(/opt/homebrew/bin/brew shellenv)" || true
+declare eval_brew=""
+
+# If Homebrew/Linuxbrew is installed, do the thing.
+if [[ -x /opt/homebrew/bin/brew ]]; then
+  eval_brew="$(/opt/homebrew/bin/brew shellenv)"
+elif [[ -x /home/linuxbrew/.linuxbrew/bin/brew ]]; then
+  eval_brew="$(/home/linuxbrew/.linuxbrew/bin/brew shellenv)"
 fi
+
+# Even when neither of the 'if's come true, 'eval'ing an empty string has no effect.
+eval "$eval_brew"
 
 # If we start using other package managers on other non-macOS systems, we will have a way in available here.
 export package_manager_prefix="${HOMEBREW_PREFIX:-"/usr/local"}"
