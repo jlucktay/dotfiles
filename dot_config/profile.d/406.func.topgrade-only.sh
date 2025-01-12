2b68105b19cd91df858ad16592e43c68dc49a0c6
James Lucktaylor <jlucktay+github@gmail.com>
2025-01-12 16:13:49 +0000

    feat(func/topgrade_only): add Bash completion

--- dot_config/profile.d/406.func.topgrade-only.sh
+++ dot_config/profile.d/406.func.topgrade-only.sh
@@ -1,44 +1,61 @@
-if command -v topgrade &> /dev/null; then
-  function topgrade_only() {
-    local -
-    set -o pipefail
+if ! command -v topgrade &> /dev/null; then
+  return 0
+fi
 
-    yq_output=$(yq --output-format=yaml '.misc.only' "$HOME"/.config/topgrade.toml)
-    mapfile -t topgrade_misc_only <<< "$yq_output"
+function topgrade_only() {
+  local -
+  set -o pipefail
 
-    only="${1:?"must pass an argument for the 'only' Topgrade section to run, valid options:
+  yq_output=$(yq --output-format=yaml '.misc.only' "$HOME"/.config/topgrade.toml)
+  mapfile -t topgrade_misc_only <<< "$yq_output"
+
+  only="${1:?"must pass an argument for the 'only' Topgrade section to run, valid options:
 $yq_output"}"
 
-    # Strip '- ' prefix from every element.
-    topgrade_misc_only=("${topgrade_misc_only[@]#'- '}")
+  # Strip '- ' prefix from every element.
+  topgrade_misc_only=("${topgrade_misc_only[@]#'- '}")
 
-    declare only_found_in_config=0
+  declare only_found_in_config=0
 
-    for i in "${topgrade_misc_only[@]}"; do
-      if [[ $i == "$only" ]]; then
-        only_found_in_config=1
-      fi
-    done
-
-    if [[ $only_found_in_config -eq 0 ]]; then
-      echo >&2 "'$only' is not set in the '.misc.only' section of the Topgrade config file."
-      echo >&2 "Valid options are:"
-      echo >&2 "$yq_output"
-
-      return 1
+  for i in "${topgrade_misc_only[@]}"; do
+    if [[ $i == "$only" ]]; then
+      only_found_in_config=1
     fi
+  done
 
-    declare -a topgrade_disable_flags=()
+  if [[ $only_found_in_config -eq 0 ]]; then
+    echo >&2 "'$only' is not set in the '.misc.only' section of the Topgrade config file."
+    echo >&2 "Valid options are:"
+    echo >&2 "$yq_output"
 
-    for ((i = 0; i < ${#topgrade_misc_only[@]}; i++)); do
-      if [[ ${topgrade_misc_only[i]} != "$only" ]]; then
-        topgrade_disable_flags+=(--disable="${topgrade_misc_only[i]}")
-      fi
-    done
+    return 1
+  fi
 
-    set -x
-    topgrade --only="$only" "${topgrade_disable_flags[@]}"
-  }
+  declare -a topgrade_disable_flags=()
 
-  export -f topgrade_only
+  for ((i = 0; i < ${#topgrade_misc_only[@]}; i++)); do
+    if [[ ${topgrade_misc_only[i]} != "$only" ]]; then
+      topgrade_disable_flags+=(--disable="${topgrade_misc_only[i]}")
+    fi
+  done
+
+  set -x
+  topgrade --only="$only" "${topgrade_disable_flags[@]}"
+}
+
+export -f topgrade_only
+
+if ! command -v yq &> /dev/null; then
+  return 0
 fi
+
+function _topgrade_only() {
+  yq_misc_only=$(yq --output-format=yaml '.misc.only' "$HOME"/.config/topgrade.toml)
+
+  # Strip '- ' prefix from every element using '${parameter//pattern/string}' expansion.
+  compgen_yq_misc_only=$(compgen -W "${yq_misc_only//'- '/''}" -- "$2")
+
+  mapfile -t COMPREPLY <<< "$compgen_yq_misc_only"
+}
+
+complete -F _topgrade_only topgrade_only
