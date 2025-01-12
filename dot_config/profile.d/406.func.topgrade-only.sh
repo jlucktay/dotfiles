e80db97defec3c2e5258c6d0f56b380f09a1831f
James Lucktaylor <jlucktay+github@gmail.com>
2025-01-12 05:09:49 +0000

    fix(func/topgrade_only): use for loop to search array for exact match, and print options in unset flag error case

--- dot_config/profile.d/406.func.topgrade-only.sh
+++ dot_config/profile.d/406.func.topgrade-only.sh
@@ -3,15 +3,24 @@ if command -v topgrade &> /dev/null; then
     local -
     set -o pipefail
 
-    only="${1:?'must pass an argument for the "only" Topgrade section to run'}"
-
     yq_output=$(yq --output-format=yaml '.misc.only' "$HOME"/.config/topgrade.toml)
     mapfile -t topgrade_misc_only <<< "$yq_output"
+
+    only="${1:?"must pass an argument for the 'only' Topgrade section to run, valid options:
+$yq_output"}"
 
     # Strip '- ' prefix from every element.
     topgrade_misc_only=("${topgrade_misc_only[@]#'- '}")
 
-    if ! [[ ${topgrade_misc_only[*]} =~ $only ]]; then
+    declare only_found_in_config=0
+
+    for i in "${topgrade_misc_only[@]}"; do
+      if [[ $i == "$only" ]]; then
+        only_found_in_config=1
+      fi
+    done
+
+    if [[ $only_found_in_config -eq 0 ]]; then
       echo >&2 "'$only' is not set in the '.misc.only' section of the Topgrade config file."
       echo >&2 "Valid options are:"
       echo >&2 "$yq_output"
