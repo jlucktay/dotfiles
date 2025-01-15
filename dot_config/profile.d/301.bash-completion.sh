f4c233cccb87448b7158ef8305e73b3c461d8357
James Lucktaylor <jlucktay+github@gmail.com>
2025-01-15 13:32:42 +0000

    refactor(bash-completion): clean up and consolidate

--- dot_config/profile.d/301.bash-completion.sh
+++ dot_config/profile.d/301.bash-completion.sh
@@ -12,19 +12,9 @@ if command -v git &> /dev/null && [[ -r "$HOME/git-completion.bash" ]]; then
   source "$HOME/git-completion.bash"
 fi
 
-# Jira CLI
-if command -v jira &> /dev/null; then
-  jira_completion_bash=$(jira completion bash)
-
-  # shellcheck source=/dev/null
-  source <(echo "$jira_completion_bash")
-
-  unset jira_completion_bash
-fi
-
 # Kubernetes CLI, via mise.
 # This is a kludge around Rancher Desktop still being in front (PATH-wise) of mise at this stage of shell initialisation.
-# There is a 'touch' command to bump the mise global config file and trigger the aggressive PATH-first behaviour.
+# There is a 'touch' command to bump the mise global config file and trigger mise's 'activate_aggressive' PATH-first behaviour.
 # However, that won't take effect until a prompt appears and the mise hook is fired.
 if mwk=$(mise which kubectl); then
   kubectl_completion_bash=$($mwk completion bash)
@@ -38,43 +28,60 @@ if mwk=$(mise which kubectl); then
   complete -o default -F __start_kubectl k
 fi
 
-# Nomad CLI
-if [[ -x "${package_manager_prefix:?}/bin/nomad" ]]; then
-  complete -C "${package_manager_prefix:?}/bin/nomad" nomad
-fi
-
-# SeaweedFS
-if [[ -x "${package_manager_prefix:?}/bin/weed" ]]; then
-  complete -C "${package_manager_prefix:?}/bin/weed" weed
-fi
-
-# tabtab
-if [[ -r "$HOME/.config/tabtab/bash/__tabtab.bash" ]]; then
-  # shellcheck disable=SC1091
-  source "$HOME/.config/tabtab/bash/__tabtab.bash"
-fi
-
 # Terraform
-if [[ -x "${package_manager_prefix:?}/bin/terraform" ]]; then
-  complete -C "${package_manager_prefix:?}/bin/terraform" terraform
+if cmd_terraform=$(command -v terraform); then
+  complete -C "$cmd_terraform" terraform
 fi
 
-# Timoni
-if command -v timoni > /dev/null; then
-  timoni_completion_bash=$(timoni completion bash)
+declare -a tools_completion_bash=(
+  "cobra-cli completion bash 2> /dev/null"
+  "docker completion bash"
+  "gup completion bash"
+  "helm completion bash"
+  "jira completion bash"
+  "jj util completion bash"
+  "kondo --completions bash 2> /dev/null"
+  "miniserve --print-completions bash"
+  "mise completion bash"
+  "nerdctl completion bash"
+  "octocov completion bash"
+  "op completion bash"
+  "pnpm completion bash"
+  "rclone completion bash -"
+  "rdctl completion bash"
+  "rustup completions bash cargo"
+  "rustup completions bash rustup"
+  "starship completions bash"
+  "terraform-docs completion bash"
+  "warp-cli generate-completions bash"
+)
 
+for tcb in "${tools_completion_bash[@]}"; do
+  # Split the command up into an array with 'read' and the internal field separator set to a space.
+  IFS=' ' read -ra arrTCB <<< "$tcb"
+
+  # The first element/word is the command generating the completion.
+  if ! command -v "${arrTCB[0]}" > /dev/null; then
+    echo >&2 "${BASH_SOURCE[0]}:$LINENO: could not find command '${arrTCB[0]}'"
+
+    continue
+  fi
+
+  # Attempt to capture the tool's Bash completion script.
+  if ! tool_completion_bash=$(eval "$tcb"); then
+    echo >&2 "${BASH_SOURCE[0]}:$LINENO: could not capture completion script for command '${arrTCB[0]}'"
+
+    continue
+  fi
+
+  # Execute the tool's captured completion script.
+  # Note that 'source' must see the script variable as a file using '<(echo ...)' process substitution.
+  # We cannot use a '<<<' here string to supply the value of the variable directly.
   # shellcheck source=/dev/null
-  source <(echo "$timoni_completion_bash")
+  source <(echo "$tool_completion_bash")
 
-  unset timoni_completion_bash
-fi
+  # Clear the captured completion script.
+  unset tool_completion_bash
+done
 
-# Warp CLI - Cloudflare
-if command -v warp-cli > /dev/null; then
-  warp_cli_generate_completions_bash=$(warp-cli generate-completions bash)
-
-  # shellcheck source=/dev/null
-  source <(echo "$warp_cli_generate_completions_bash")
-
-  unset warp_cli_generate_completions_bash
-fi
+unset tools_completion_bash
