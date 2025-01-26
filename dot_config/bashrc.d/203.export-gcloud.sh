9052c602357b60e355dd7078f9454c197251ef48
James Lucktaylor <jlucktay+github@gmail.com>
2025-01-26 14:38:56 +0000

    perf(bashrc.d/203): optimise a little by taking jq out of the loop

--- dot_config/bashrc.d/203.export-gcloud.sh
+++ dot_config/bashrc.d/203.export-gcloud.sh
@@ -1,11 +1,12 @@
 # Point the environment variable for the Application Default Credentials file at the correct one based on the currently
 # active config in the Google Cloud CLI.
-if command -v gcloud &> /dev/null && command -v jq &> /dev/null; then
-  config_list=$(gcloud config configurations list --format=json)
-  active_config=$(jq --raw-output '.[] | select( .is_active == true ) | .name' <<< "$config_list")
-  adc_file="$HOME/.config/gcloud/application_default_credentials.$active_config.json"
+if command -v gcloud &> /dev/null; then
+  _active_config=$(gcloud config configurations list --filter="is_active:true" --format="value(name)")
+  _adc_file="$HOME/.config/gcloud/application_default_credentials.$_active_config.json"
 
-  if [[ -r $adc_file ]]; then
-    export GOOGLE_APPLICATION_CREDENTIALS="$adc_file"
+  if [[ -r $_adc_file ]]; then
+    export GOOGLE_APPLICATION_CREDENTIALS="$_adc_file"
   fi
+
+  unset _active_config _adc_file
 fi
