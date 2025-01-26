36fda06eec839b74e541b75d396a70d4fd7f7319
James Lucktaylor <jlucktay+github@gmail.com>
2025-01-26 14:44:07 +0000

    perf(bashrc.d/203): optimise a lot by not even running gcloud and just reading straight from a file

--- dot_config/bashrc.d/203.export-gcloud.sh
+++ dot_config/bashrc.d/203.export-gcloud.sh
@@ -1,12 +1,14 @@
-# Point the environment variable for the Application Default Credentials file at the correct one based on the currently
-# active config in the Google Cloud CLI.
+# Point the environment variable for the Application Default Credentials file at the correct one based on the Google Cloud CLI's currently active config.
 if command -v gcloud &> /dev/null; then
-  _active_config=$(gcloud config configurations list --filter="is_active:true" --format="value(name)")
-  _adc_file="$HOME/.config/gcloud/application_default_credentials.$_active_config.json"
+  if _active_config=$(< "$HOME/.config/gcloud/active_config"); then
+    _adc_file="$HOME/.config/gcloud/application_default_credentials.$_active_config.json"
 
-  if [[ -r $_adc_file ]]; then
-    export GOOGLE_APPLICATION_CREDENTIALS="$_adc_file"
+    if [[ -r $_adc_file ]]; then
+      export GOOGLE_APPLICATION_CREDENTIALS="$_adc_file"
+    fi
+
+    unset _adc_file
   fi
 
-  unset _active_config _adc_file
+  unset _active_config
 fi
