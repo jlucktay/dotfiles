06b77a7c05a0406c0a703033ec1535c292a7ea30
James Lucktaylor <jlucktay+github@gmail.com>
2025-04-02 09:41:45 +0100

    fix(bin/morning.sh): don't use negative numbers for bat's line range

--- bin/executable_morning.sh
+++ bin/executable_morning.sh
@@ -91,7 +91,7 @@ rdu_logfile="$HOME"/Library/Logs/rancher-desktop/update.log
 
 if [[ -r $rdu_logfile ]]; then
   rdu_lines=$(wc -l < "$rdu_logfile" | xargs)
-  rdu_range="$((rdu_lines - 10)):$rdu_lines"
+  rdu_range="$((rdu_lines < 10 ? 0 : rdu_lines - 10)):$rdu_lines"
 
   command_queue+=(
     # Check for any pending updates to RD; requires a restart of the app and its VM.
