b27b165db38270fb276cb7abdd7e76eec150fdf3
James Lucktaylor <jlucktay+github@gmail.com>
2025-06-03 09:28:13 +0100

    feat(bin): output missing resource type(s) alongside container

--- bin/executable_k8s-missing-resource-defs.sh
+++ bin/executable_k8s-missing-resource-defs.sh
@@ -1,4 +1,4 @@
 #!/usr/bin/env bash
 set -euo pipefail
 
-kubectl get pod --all-namespaces --output=go-template-file="$HOME/etc/k8s-container-resources.gotmpl"
+kubectl get pod --all-namespaces --output=go-template-file="$HOME/etc/k8s-missing-resource-defs.gotmpl"
