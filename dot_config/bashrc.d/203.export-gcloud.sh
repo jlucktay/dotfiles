7ac825ad3c94ccaa12f185ccb12d4254959a6f81
James Lucktaylor <jlucktay+github@gmail.com>
2025-08-29 14:24:51 +0100

    fix(.config/bashrc.d/gcloud): add a check for ZTNA custom CA

--- dot_config/bashrc.d/203.export-gcloud.sh
+++ dot_config/bashrc.d/203.export-gcloud.sh
@@ -10,5 +10,21 @@ if command -v gcloud &> /dev/null; then
     unset _adc_file
   fi
 
+  if [[ $_active_config == "ovo" ]]; then
+    _gc_cfg_cccf_empty=0
+
+    if ! _gc_cfg_cccf=$(gcloud config get core/custom_ca_certs_file --format=json 2> /dev/null); then
+      _gc_cfg_cccf_empty=1
+    elif [[ -z $_gc_cfg_cccf ]] || [[ $_gc_cfg_cccf == '[]' ]]; then
+      _gc_cfg_cccf_empty=1
+    fi
+
+    if [[ $_gc_cfg_cccf_empty -eq 1 ]]; then
+      echo >&2 "❌ the 'core/custom_ca_certs_file' config needs to be set for 'gcloud': https://ovotech.atlassian.net/wiki/spaces/OTKB/pages/4596302401"
+    fi
+
+    unset _gc_cfg_cccf_empty _gc_cfg_cccf
+  fi
+
   unset _active_config
 fi
