74b9aa407a574ae2eb078deb20db3244df1061b2
James Lucktaylor <jlucktay+github@gmail.com>
2025-10-05 11:55:45 +0100

    refactor: 💡 xdg
    Redo some XDG directory configs.

--- dot_config/bashrc.d/202.export-cache.sh
+++
@@ -1,3 +0,0 @@
-export XDG_CACHE_HOME="$HOME/Library/Caches/org.freedesktop"
-
-mkdir -p "$XDG_CACHE_HOME"
---
+++ dot_config/bashrc.d/202.export-xdg.sh
@@ -0,0 +1,7 @@
+# Define XDG Base directory environment variables.
+export XDG_CACHE_HOME="$HOME/.cache"
+export XDG_CONFIG_HOME="$HOME/.config"
+export XDG_DATA_HOME="$HOME/.local/share"
+export XDG_STATE_HOME="$HOME/.local/state"
+
+mkdir -p "$XDG_CACHE_HOME"
