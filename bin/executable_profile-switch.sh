2c4e7bbdcd9792c8cac8aa747f65c479277c4ba1
James Lucktaylor <jlucktay+github@gmail.com>
2026-01-12 11:50:10 +0000

    feat: 🎸 New script w/ completions to switch Chezmoi profile

---
+++ bin/executable_profile-switch.sh
@@ -0,0 +1,12 @@
+#!/usr/bin/env bash
+set -euo pipefail
+
+export profile=${1:?}
+
+set -x
+
+yq --inplace '.profile = strenv(profile)' "$HOME/.local/share/chezmoi/.chezmoidata.yaml"
+task --dir "$HOME/.local/share/chezmoi/" --taskfile "$HOME/.local/share/chezmoi/Taskfile.chezmoi.yaml" data
+
+ln -fsv "application_default_credentials.$profile.json" "$HOME/.config/gcloud/application_default_credentials.json"
+gcloud config configurations activate "$profile"
