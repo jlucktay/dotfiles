2048899607f04ddca23a23fcf440cabb1d274f13
James Lucktaylor <jlucktay+github@gmail.com>
2026-01-14 20:52:59 +0000

    fix: 🐛 Remove outmoded logic for gcloud ADC

--- dot_config/bashrc.d/203.export-gcloud.sh
+++ dot_config/bashrc.d/203.export-gcloud.sh
@@ -1,13 +1,7 @@
 # Point the environment variable for the Application Default Credentials file at the correct one based on the Google Cloud CLI's currently active config.
 if command -v gcloud &> /dev/null; then
-  if _active_config=$(< "$HOME/.config/gcloud/active_config"); then
-    _adc_file="$HOME/.config/gcloud/application_default_credentials.$_active_config.json"
-
-    if [[ -r $_adc_file ]]; then
-      export GOOGLE_APPLICATION_CREDENTIALS="$_adc_file"
-    fi
-
-    unset _adc_file
+  if ! _active_config=$(< "$HOME/.config/gcloud/active_config"); then
+    return 0
   fi
 
   if [[ $_active_config == "ovo" ]]; then
