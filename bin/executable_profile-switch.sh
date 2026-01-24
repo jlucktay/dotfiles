e94bc97d4bf2f1a0657020a5b927c217108601d3
James Lucktaylor <jlucktay+github@gmail.com>
2026-01-24 13:13:52 +0000

    refactor: 💡 Profile things

--- bin/executable_profile-switch.sh
+++ bin/executable_profile-switch.sh
@@ -1,14 +1,14 @@
 #!/usr/bin/env bash
 set -euo pipefail
 
-export profile=${1:?}
+declare -rx PROFILE=${1:?}
 
 set -x
 
-yq --inplace '.profile = strenv(profile)' "$HOME/.local/share/chezmoi/.chezmoidata.yaml"
-task --dir "$HOME/.local/share/chezmoi/" --taskfile "$HOME/.local/share/chezmoi/Taskfile.chezmoi.yaml" data
+yq --inplace '.profile = strenv(PROFILE)' "$HOME/.local/share/chezmoi/.chezmoidata.yaml"
+task --taskfile "$HOME/.local/share/chezmoi/Taskfile.chezmoi.yaml" data
 
-ln -fsv "application_default_credentials.$profile.json" "$HOME/.config/gcloud/application_default_credentials.json"
-gcloud config configurations activate "$profile"
+ln -fsv "application_default_credentials.$PROFILE.json" "$HOME/.config/gcloud/application_default_credentials.json"
+gcloud config configurations activate "$PROFILE"
 
 chezmoi apply "$HOME/.config/aws/config" "$HOME/.terraform.d/credentials.tfrc.json" "$HOME/.terraformrc"
