16a4a80f13417c52ea7e6aae0c4889524df6b934
James Lucktaylor <jlucktay+github@gmail.com>
2026-04-23 10:44:09 +0100

    feat: 🎸 Set up autocomplete for K8s namespace switching

--- dot_config/profile.d/418.func.kc.sh
+++ dot_config/profile.d/418.func.kc.sh
@@ -1,4 +1,4 @@
-if ! command -v kc &> /dev/null; then
+if ! command -v kc &> /dev/null || ! command -v kn &> /dev/null; then
 	return 0
 fi
 
@@ -7,7 +7,7 @@ function _kubecm_context() {
 	set -o pipefail
 
 	# Get valid values for the context name from kubeconfig.
-	context_names=$(kubectl config view | yq --exit-status '.contexts[].name')
+	context_names=$(kubectl config view --output=jsonpath='{range .contexts[*]}{.name}{"\n"}{end}')
 
 	# Generate possible completions with the context names.
 	compgen_context_names=$(compgen -W "$context_names" -- "$2")
@@ -17,3 +17,27 @@ function _kubecm_context() {
 }
 
 complete -F _kubecm_context kc
+
+function _kubecm_namespace() {
+	local -
+	set -o pipefail
+
+	# Get valid namespaces from the current cluster.
+	if ! namespaces=$(kubectl get namespaces --output=jsonpath='{range .items[*]}{.metadata.name}{"\n"}{end}' --request-timeout=3s 2> /dev/null); then
+		if ! kubectl config view --minify &> /dev/null; then
+			mapfile -t COMPREPLY <<< "no-context-set"
+		else
+			mapfile -t COMPREPLY <<< "unknown-error"
+		fi
+
+		return
+	fi
+
+	# Generate possible completions with the namespaces.
+	compgen_namespaces=$(compgen -W "$namespaces" -- "$2")
+
+	# Store possible completions for 'complete' to make use of.
+	mapfile -t COMPREPLY <<< "$compgen_namespaces"
+}
+
+complete -F _kubecm_namespace kn
